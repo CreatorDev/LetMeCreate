b2498534a2aa8e7db6f9460496d5d3a830e80cba
Francois Berder <fberder@outlook.fr>
2016-05-12 22:34:09 +0100

    led: switch off all leds on initialization
    Signed-off-by: Francois Berder <fberder@outlook.fr>

--- src/core/led.c
+++ src/core/led.c
@@ -47,6 +47,15 @@ int led_initialize(void)
         }
     }
 
+    /* Switch off all LEDS */
+    led_switch_off(LED_0
+                 | LED_1
+                 | LED_2
+                 | LED_3
+                 | LED_4
+                 | LED_5
+                 | LED_6);
+
     return 0;
 }
 
