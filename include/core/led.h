964586d28e141cd6be91e3b988a932361b1ce65f
Francois Berder <fberder@outlook.fr>
2016-05-13 21:38:04 +0100

    led: add timer mode
    Signed-off-by: Francois Berder <fberder@outlook.fr>

--- include/core/led.h
+++ include/core/led.h
@@ -22,7 +22,9 @@ typedef enum LED_INDEX {
 } LED_INDEX;
 
 /**
- * @brief Initialise file descriptors for each LED. Switch off all LEDs.
+ * @brief Initialise file descriptors for each LED. Configure all LEDS in on/off mode. Switch off 
+ * all LEDs.
+ *
  * @return 0 if successful, -1 otherwise
  */
 int led_init(void);
@@ -58,6 +60,31 @@ int led_switch_off(const uint8_t mask);
  * @return 0 if successful, -1 otherwise
  */
 int led_set(const uint8_t mask, const uint8_t value);
+
+/**
+ * @brief Configure all LEDS from mask in on/off mode. led_init must have been called before.
+ *
+ * @param[in] mask
+ * @return 0 if successful, -1 otherwise
+ */
+int led_configure_on_off_mode(const uint8_t mask);
+
+/**
+ * @brief Configure all LEDS from mask in timer mode. led_init must have been called before.
+ *
+ * @param[in] mask
+ * @return 0 if successful, -1 otherwise
+ */
+int led_configure_timer_mode(const uint8_t mask);
+
+/**
+ * @brief Configure delays for LEDS. LEDS must have been configured in timer mode before.
+ *
+ * @param[in] delay_on Defines how long the LED will stay on (in milliseconds)
+ * @param[in] delay_off Defines how long the LED will stay on (in milliseconds)
+ * @return 0 if successful, -1 otherwise
+ */
+int led_set_delay(const uint8_t mask, const uint32_t delay_on, const uint32_t delay_off);
 
 /**
  * @brief Close file descriptors for each LED.
--- src/core/led.c
+++ src/core/led.c
@@ -13,7 +13,39 @@
 
 int fds[NB_LEDS] = { -1, -1, -1, -1, -1, -1, -1, -1 };
 
-static int led_set_value(const uint8_t led_index, const uint8_t value)
+static int build_file_path(char *path, const uint8_t led_index, const char *filename)
+{
+    if (led_index >= NB_LEDS) {
+        fprintf(stderr, "led: Cannot build device file path for invalid led %d\n", led_index);
+        return -1;
+    }
+
+    if (path == NULL) {
+        fprintf(stderr, "led: Cannot store device file path for led %d to null variable\n", led_index);
+        return -1;
+    }
+
+    if (filename == NULL) {
+        fprintf(stderr, "led: Cannot build device file path for led %d with null filename\n", led_index);
+        return -1;
+    }
+
+    if(led_index == 7) {
+        if (sprintf(path, LED_HEARTBEAT_DEVICE_FILE_PATH, filename) < 0) {
+            fprintf(stderr, "led: Failed to build %s device file path for led heartbeat\n", filename);
+            return -1;
+        }
+    } else {
+        if (sprintf(path, LED_DEVICE_FILE_PATH, led_index+1, filename) < 0) {
+            fprintf(stderr, "led: Failed to build %s device file path for led %d\n", filename, led_index);
+            return -1;
+        }
+    }
+
+    return 0;
+}
+
+static int set_value(const uint8_t led_index, const uint8_t value)
 {
     char *str = NULL;
 
@@ -28,31 +60,53 @@ static int led_set_value(const uint8_t led_index, const uint8_t value)
     return write(fds[led_index], str, 2);
 }
 
-static int led_set_mode(const uint8_t led_index, const char *mode)
+static int set_mode(const uint8_t led_index, const char *mode)
 {
     int fd = -1;
     char path[255];
 
-    if(led_index == 7) {
-        if (snprintf(path, sizeof(path), LED_HEARTBEAT_DEVICE_FILE_PATH, "trigger") < 0) {
-            fprintf(stderr, "led: Failed to build device file path for led heartbeat\n");
-            return -1;
-        }
-    } else {
-        if (snprintf(path, sizeof(path), LED_DEVICE_FILE_PATH, led_index+1, "trigger") < 0) {
-            fprintf(stderr, "led: Failed to build device file path for led %d\n", led_index);
-            return -1;
-        }
-    }
+    if (build_file_path(path, led_index, "trigger") < 0)
+        return -1;
 
     if ((fd = open(path, O_WRONLY)) < 0) {
         fprintf(stderr, "led: Failed to open trigger device file for led %d\n", led_index);
         return -1;
     }
     if (write(fd, mode, strlen(mode)+1) < 0) {
+        close(fd);
         fprintf(stderr, "led: Failed to set mode to %s for led %d\n", mode, led_index);
         return -1;
     }
+    close(fd);
+
+    return 0;
+}
+
+static int set_delay(const uint8_t led_index, const char *filename, const uint32_t value)
+{
+    int fd = -1;
+    char path[255];
+    char str[255];
+
+    if (build_file_path(path, led_index, filename) < 0)
+        return -1;
+
+    if (snprintf(str, sizeof(str), "%d", value) < 0) {
+        fprintf(stderr, "led: Failed to convert unsigned integer into string\n");
+        return -1;
+    }
+
+    if ((fd = open(path, O_WRONLY)) < 0) {
+        fprintf(stderr, "led: Failed to open %s device file for led %d\n", filename, led_index);
+        return -1;
+    }
+
+    if (write(fd, str, strlen(str)+1) < 0) {
+        fprintf(stderr, "led: Failed to set value for %s for led %d\n", filename, led_index);
+        close(fd);
+        return -1;
+    }
+
     close(fd);
 
     return 0;
@@ -65,23 +119,14 @@ int led_init(void)
     for (; i < NB_LEDS; ++i) {
         char path[255];
 
-        if (led_set_mode(i, "none") < 0)
+        if (set_mode(i, "none") < 0)
             return -1;
 
         if (fds[i] >= 0)
             continue;
 
-        if(i == 7) {
-            if (snprintf(path, sizeof(path), LED_HEARTBEAT_DEVICE_FILE_PATH, "brightness") < 0) {
-                fprintf(stderr, "led: Failed to build device file path for led heartbeat\n");
-                return -1;
-            }
-        } else {
-            if (snprintf(path, sizeof(path), LED_DEVICE_FILE_PATH, i+1, "brightness") < 0) {
-                fprintf(stderr, "led: Failed to build device file path for led %d\n", i);
-                return -1;
-            }
-        }
+        if (build_file_path(path, i, "brightness") < 0)
+            return -1;
 
         fds[i] = open(path, O_WRONLY);
         if (fds[i] < 0) {
@@ -120,10 +165,60 @@ int led_set(const uint8_t mask, const uint8_t value)
         if ((mask & tmp) == 0)
             continue;
 
-        if (led_set_value(i, value & mask) < 0) {
+        if (set_value(i, value & mask) < 0) {
             fprintf(stderr, "Failed to switch %s led %d\n", (value & mask) ? "on" : "off", i);
             return -1;
         }
+    }
+
+    return 0;
+}
+
+int led_configure_on_off_mode(const uint8_t mask)
+{
+    int i = 0, tmp = 1;
+
+    for (; i < NB_LEDS; ++i, tmp <<= 1) {
+        if ((mask & tmp) == 0)
+            continue;
+
+        if (set_mode(i, "none") < 0) {
+            fprintf(stderr, "led: Failed to configure led %d in on/off mode\n", i);
+            return -1;
+        } 
+    }
+
+    return 0;
+}
+
+int led_configure_timer_mode(const uint8_t mask)
+{
+    int i = 0, tmp = 1;
+
+    for (; i < NB_LEDS; ++i, tmp <<= 1) {
+        if ((mask & tmp) == 0)
+            continue;
+
+        if (set_mode(i, "timer") < 0) {
+            fprintf(stderr, "led: Failed to configure led %d in timer mode\n", i);
+            return -1;
+        }
+    }
+
+    return 0;
+}
+
+int led_set_delay(const uint8_t mask, const uint32_t delay_on, const uint32_t delay_off)
+{
+    int i = 0, tmp = 1;
+
+    for (; i < NB_LEDS; ++i, tmp <<= 1) {
+        if ((mask & tmp) == 0)
+            continue;
+
+        if (set_delay(i, "delay_on", delay_on) < 0
+        ||  set_delay(i, "delay_off", delay_off) < 0)
+            return -1;
     }
 
     return 0;
