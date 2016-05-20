b55dc39009214bcf87eeefb38c4bbbe562bde971
Francois Berder <fberder@outlook.fr>
2016-05-20 22:59:43 +0100

    gpio: add documentation
    Signed-off-by: Francois Berder <fberder@outlook.fr>

--- include/core/gpio.h
+++ include/core/gpio.h
@@ -26,11 +26,60 @@ enum GPIO_DIR {
     GPIO_INPUT
 };
 
+/**
+ * @brief Export a GPIO.
+ *
+ * This function must be called before doing any operation on this GPIO (except releasing it).
+ *
+ * @param[in] gpio_pin Index of the GPIO
+ * @return 0 if succesful, -1 otherwise
+ */
 int gpio_init(const uint8_t gpio_pin);
+
+/**
+ * @brief Configure GPIO as input or output.
+ *
+ * @param[in] gpio_pin Index of the GPIO
+ * @param[in] dir Direction of the gpio (must be GPIO_OUTPUT or GPIO_INPUT)
+ * @return 0 if succesful, -1 otherwise
+ */
 int gpio_set_direction(const uint8_t gpio_pin, const uint8_t dir);
+
+/**
+ * @brief Get the direction of a GPIO.
+ *
+ * @param[in] gpio_pin Index of the GPIO
+ * @param[out] dir Pointer to a variable (must not be null)
+ * @return 0 if succesful, -1 otherwise
+ */
 int gpio_get_direction(const uint8_t gpio_pin, uint8_t *dir);
+
+/**
+ * @brief Set the output state (high or low) of a GPIO
+ *
+ * The GPIO must be configured as an output.
+ *
+ * @param[in] gpio_pin Index of the GPIO
+ * @param[in] value (0: low, any other value: high)
+ * @return 0 if succesful, -1 otherwise
+ */
 int gpio_set_value(const uint8_t gpio_pin, const uint8_t value);
+
+/**
+ * @brief Get the output state of a GPIO.
+ *
+ * @param[in] gpio_pin Index of the GPIO
+ * @param[out] value Pointer to a variable (must not be null)
+ * @return 0 if succesful, -1 otherwise
+ */
 int gpio_get_value(const uint8_t gpio_pin, uint8_t *value);
+
+/**
+ * @brief Unexport a GPIO.
+ *
+ * @param gpio_pin Index of the GPIO
+ * @return 0 if succesful, -1 otherwise
+ */
 int gpio_release(const uint8_t gpio_pin);
 
 #endif
