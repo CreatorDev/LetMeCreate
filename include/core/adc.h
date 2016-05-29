ef3bcf1aaab54a0a238658fc639655036006433b
Francois Berder <fberder@outlook.fr>
2016-05-29 15:09:51 +0100

    adc: add documentation
    Signed-off-by: Francois Berder <fberder@outlook.fr>

--- include/core/adc.h
+++ include/core/adc.h
@@ -11,11 +11,19 @@
 
 #include <stdint.h>
 
+/** Index of ADC */
 enum ADC_INDEX {
-    MIKROBUS_1_ADC,
-    MIKROBUS_2_ADC
+    MIKROBUS_1_ADC, /**< ADC available on Mikrobus 1 and Raspberry pi interface */
+    MIKROBUS_2_ADC  /**< ADC available on Mikrobus 2 and Raspberry pi interface */
 };
 
+/**
+ * @brief Get the reading of an ADC in Volt, in range 0-5V.
+ *
+ * @param[in] index Index of the ADC (see #ADC_INDEX)
+ * @param[out] value Pointer to a floating point variable (must be non-null)
+ * @return 0 if succesful, -1 otherwise
+ */
 int adc_get_value(const uint8_t index, float *value);
 
 #endif
