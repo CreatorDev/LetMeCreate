c031aeb3f924d6b48ff77625228e09a093fd3c48
Francois Berder <fberder@outlook.fr>
2016-06-07 19:04:38 +0100

    relay2: add example
    Signed-off-by: Francois Berder <fberder@outlook.fr>

---
+++ examples/relay2/main.c
@@ -0,0 +1,31 @@
+#include <unistd.h>
+#include "click/relay2.h"
+#include "core/common.h"
+
+int main(void)
+{
+    int counter = 0;
+    while (1) {
+        switch (counter % 4) {
+        case 0:
+            relay2_click_disable_relay_1(MIKROBUS_1);
+            relay2_click_disable_relay_2(MIKROBUS_1);
+            break;
+        case 1:
+            relay2_click_enable_relay_1(MIKROBUS_1);
+            relay2_click_disable_relay_2(MIKROBUS_1);
+            break;
+        case 2:
+            relay2_click_disable_relay_1(MIKROBUS_1);
+            relay2_click_enable_relay_2(MIKROBUS_1);
+            break;
+        case 3:
+            relay2_click_enable_relay_1(MIKROBUS_1);
+            relay2_click_enable_relay_2(MIKROBUS_1);
+            break;
+        }
+
+        sleep (1);
+        ++counter;
+    }
+}
