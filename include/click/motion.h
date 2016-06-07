efa5de51c51aab3d7a09ac719f3d7a7d32120edc
Francois Berder <fberder@outlook.fr>
2016-06-07 18:37:14 +0100

    motion: add documentation
    Signed-off-by: Francois Berder <fberder@outlook.fr>

--- include/click/motion.h
+++ include/click/motion.h
@@ -11,8 +11,31 @@
 #include <stdint.h>
 
 
+/**
+ * @brief Enable the Motion Click.
+ *
+ * @param[in] mikrobus_index Index of the mikrobus used by the click board (see #MIKROBUS_INDEX)
+ * @return 0 if successful, -1 otherwise
+ */
 int motion_click_enable(const uint8_t mikrobus_index);
+
+/**
+ * @brief Attach a callback if movement is detected.
+ *
+ * Notice that you can attach multiple callbacks.
+ *
+ * @param[in] mikrobus_index Index of the mikrobus used by the click board (see #MIKROBUS_INDEX)
+ * @param[in] callback Function to call if movement is detected
+ * @return ID of the callback. Use gpio_monitor_remove_callback to detach your callback.
+ */
 int motion_click_attach_callback(const uint8_t mikrobus_index, void(*callback)(uint8_t));
+
+/**
+ * @brief Disable the Motion Click.
+ *
+ * @param[in] mikrobus_index Index of the mikrobus used by the click board (see #MIKROBUS_INDEX)
+ * @return 0 if successful, -1 otherwise
+ */
 int motion_click_disable(const uint8_t mikrobus_index);
 
 #endif
