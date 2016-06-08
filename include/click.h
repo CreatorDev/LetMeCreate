c5e0d9a91015e284ca254426beaacc2b77c8bb5d
Francois Berder <fberder@outlook.fr>
2016-06-08 19:41:18 +0100

    add global headers
    Signed-off-by: Francois Berder <fberder@outlook.fr>

---
+++ include/click.h
@@ -0,0 +1,19 @@
+/**
+ * @file click.h
+ * @author Francois Berder
+ * @date 2016
+ * @copyright 3-clause BSD
+ */
+
+
+#ifndef __LETMECREATE_CLICK_H__
+#define __LETMECREATE_CLICK_H__
+
+#include "click/common.h"
+#include "click/joystick.h"
+#include "click/motion.h"
+#include "click/proximity.h"
+#include "click/relay2.h"
+#include "click/thermo3.h"
+
+#endif
---
+++ include/letmecreate.h
@@ -0,0 +1,15 @@
+/**
+ * @file letmecreate.h
+ * @author Francois Berder
+ * @date 2016
+ * @copyright 3-clause BSD
+ */
+
+
+#ifndef __LETMECREATE_H__
+#define __LETMECREATE_H__
+
+#include "core.h"
+#include "click.h"
+
+#endif
