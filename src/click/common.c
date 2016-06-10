0cefdb321db7b4ba141d647e6f190d14c5ff59d4
Francois Berder <fberder@outlook.fr>
2016-06-10 15:13:23 +0100

    click_common: remove unnecessary variables
    Signed-off-by: Francois Berder <fberder@outlook.fr>

--- src/click/common.c
+++ src/click/common.c
@@ -13,10 +13,8 @@ int i2c_write_register(const uint16_t address, const uint8_t reg_address, const 
 
 int i2c_read_register(const uint16_t address, const uint8_t reg_address, uint8_t *data)
 {
-    int ret = -1;
-
-    if ((ret = i2c_write_byte(address, reg_address)) < 0)
-        return ret;
+    if (i2c_write_byte(address, reg_address) < 0)
+        return -1;
 
     return i2c_read_byte(address, data);
 }
@@ -25,20 +23,19 @@ int i2c_read_16b_register(const uint16_t address,
                           const uint8_t reg_low_address, const uint8_t reg_high_address,
                           uint16_t *data)
 {
-    int ret = -1;
     uint8_t low = 0, high = 0;
 
-    if ((ret = i2c_read_register(address, reg_low_address, &low)) < 0)
-        return ret;
+    if (i2c_read_register(address, reg_low_address, &low) < 0)
+        return -1;
 
-    if ((ret = i2c_read_register(address, reg_high_address, &high)) < 0)
-        return ret;
+    if (i2c_read_register(address, reg_high_address, &high) < 0)
+        return -1;
 
     *data = high;
     *data <<= 8;
     *data |= low;
 
-    return *data;
+    return 0;
 }
 
 int spi_write_register(const uint8_t reg_address, const uint8_t data)
