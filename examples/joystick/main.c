555755a6a41851630789af271435e4ae1d57694b
Michal Tusnio <m.tusnio@gmail.com>
2016-06-13 11:53:57 +0100

    joystick: example updated to include review comments
    Signed-off-by: Michal Tusnio <m.tusnio@gmail.com>

--- examples/joystick/main.c
+++ examples/joystick/main.c
@@ -4,23 +4,23 @@
 #include <core/i2c.h>
 #include <click/joystick.h>
 
-// Roughly the variable that will be reached when the joystick is farthest
-// left or right
-#define OFFSET 98
+/*   Roughly the variable that will be reached when the joystick is farthest
+     left or right */
+#define OFFSET  98
 #define MAXIMUM (OFFSET * 2)
 
-// We have 7 LEDs + the heartbeat LED which we skip
-#define LED_COUNT 7
+/* We have 7 LEDs + the heartbeat LED which we skip */
+#define LED_CNT 7
 
 int get_led_mask(float perc)
 {
     int mask = 0;
-    int div = perc/(1.0f / LED_COUNT);
+    int div = perc/(1.0f / LED_CNT);
+    int i;
+    if (div > LED_CNT)
+        div = LED_CNT;
 
-    if (div > LED_COUNT)
-        div = LED_COUNT;
-
-    for (int i = 0; i < div; i++)
+    for (i = 0; i < div; i++)
         mask |= (1 << i);
 
     return mask;
@@ -32,8 +32,8 @@ int main()
     i2c_select_bus(MIKROBUS_1);
     led_init();
 
-    while (1)
-    {
+    /* Use Ctrl-C to break the loop */
+    while (1) {
         int8_t x, y;
         int mask;
 
