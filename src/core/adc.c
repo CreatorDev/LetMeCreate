e6e935f4785da0434f17cb227bed2bd962d47234
Francois Berder <fberder@outlook.fr>
2016-06-21 21:19:48 +0100

    adc: removed ADC_INDEX enumeration
    Signed-off-by: Francois Berder <fberder@outlook.fr>

--- src/core/adc.c
+++ src/core/adc.c
@@ -5,12 +5,12 @@
 #define ADC_BASE_PATH       "/sys/bus/iio/devices/iio:device0/"
 
 
-int adc_get_value(const uint8_t index, float *value)
+int adc_get_value(const uint8_t mikrobus_index, float *value)
 {
     uint32_t tmp = 0;
     char path[MAX_STR_LENGTH];
 
-    if (index != MIKROBUS_1_ADC && index != MIKROBUS_2_ADC) {
+    if (mikrobus_index != MIKROBUS_1 && mikrobus_index != MIKROBUS_2) {
         fprintf(stderr, "adc: Invalid index.\n");
         return -1;
     }
@@ -20,13 +20,13 @@ int adc_get_value(const uint8_t index, float *value)
         return -1;
     }
 
-    if (snprintf(path, MAX_STR_LENGTH, ADC_BASE_PATH"in_voltage%d_raw", index) < 0) {
-        fprintf(stderr, "adc: Failed to create path to access value of ADC %d.\n", index);
+    if (snprintf(path, MAX_STR_LENGTH, ADC_BASE_PATH"in_voltage%d_raw", mikrobus_index) < 0) {
+        fprintf(stderr, "adc: Failed to create path to access value of ADC %d.\n", mikrobus_index);
         return -1;
     }
 
     if (read_int_file(path, &tmp) < 0) {
-        fprintf(stderr, "adc: Failed to read value from ADC %d.\n", index);
+        fprintf(stderr, "adc: Failed to read value from ADC %d.\n", mikrobus_index);
         return -1;
     }
 
