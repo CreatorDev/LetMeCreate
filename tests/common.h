7f2308284a4f398df0696a217bf3f327475232f9
Francois Berder <fberder@outlook.fr>
2016-07-02 16:59:14 +0100

    common: add macros
    Signed-off-by: Francois Berder <fberder@outlook.fr>

--- tests/common.h
+++ tests/common.h
@@ -25,4 +25,21 @@ int wait_for_switch(unsigned int timeout);
 /* timeout in seconds */
 int ask_question(const char *question, unsigned int timeout);
 
+#define ADD_TEST_CASE(TEST, CASE) \
+    do { \
+        struct test_case *tmp = test_##TEST.cases; \
+        while (tmp->name[0] != 0) \
+            ++tmp; \
+        struct test_case case_##CASE = { #CASE, test_##TEST##_##CASE }; \
+        *tmp = case_##CASE; \
+    } while(0)
+
+#define CREATE_TEST(NAME, CASE_CNT) \
+    struct test test_##NAME = { \
+        .name = #NAME, \
+        .case_cnt = CASE_CNT, \
+        .cases = malloc((CASE_CNT) * sizeof(struct test_case)) \
+    }; \
+    memset(test_##NAME.cases, 0, (CASE_CNT) * sizeof(struct test_case));
+
 #endif
