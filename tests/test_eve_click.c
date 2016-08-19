958fe04472a4c0a0e6c2dd23926a7b9b1d933e9c
Francois Berder <fberder@outlook.fr>
2016-08-19 07:43:30 +0100

    test: add eve click
    Signed-off-by: Francois Berder <fberder@outlook.fr>

---
+++ tests/test_eve_click.c
@@ -0,0 +1,57 @@
+/**
+ * @author Francois Berder
+ * @date 2016
+ * @copyright 3-clause BSD
+ */
+
+#include <linux/spi/spidev.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "common.h"
+#include "letmecreate/core/spi.h"
+#include "letmecreate/click/eve.h"
+
+
+static bool test_eve_click_enable_disable(void)
+{
+    if (eve_click_enable(MIKROBUS_1) < 0
+    ||  eve_click_disable(MIKROBUS_1) < 0)
+        return false;
+
+    if (eve_click_enable(MIKROBUS_1) < 0
+    ||  eve_click_disable(MIKROBUS_1) < 0)
+        return false;
+
+    return true;
+}
+
+static bool test_eve_click_black_screen_on_enable(void)
+{
+    if (eve_click_enable(MIKROBUS_1) < 0)
+        return false;
+
+    return ask_question("Do you see a black screen ?", 15) == 1;
+}
+
+int main(void)
+{
+    int ret = -1;
+
+    CREATE_TEST(eve_click, 2)
+    ADD_TEST_CASE(eve_click, enable_disable);
+    ADD_TEST_CASE(eve_click, black_screen_on_enable);
+
+
+    if (spi_init() < 0
+    ||  spi_set_mode(MIKROBUS_1, SPI_MODE_0) < 0)
+        return -1;
+
+    ret = run_test(test_eve_click);
+    free(test_eve_click.cases);
+
+    spi_release();
+
+    return ret;
+}
+
