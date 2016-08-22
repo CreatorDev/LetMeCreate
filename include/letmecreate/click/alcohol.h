cbf911a42db2486276e998bc17830bd719a86863
Francois Berder <fberder@outlook.fr>
2016-08-22 21:59:14 +0100

    alcohol: add documentation
    Signed-off-by: Francois Berder <fberder@outlook.fr>

--- include/letmecreate/click/alcohol.h
+++ include/letmecreate/click/alcohol.h
@@ -3,6 +3,17 @@
 
 #include <stdint.h>
 
+/**
+ * @brief Get a measure from Alcohol Click.
+ *
+ * The Alcohol click has a sensor which outputs a voltage depending on the
+ * concentration of alcohol. This function returns the voltage read on ADC pin.
+ *
+ * param[in] mikrobus_index Index of the mikrobus
+ * @param[out] measure Pointer to a 16-bit variable to retrieve the output from the click (must not be null)
+ *
+ * @return 0 if successful, otherwise it returns -1.
+ */
 int alcohol_click_get_measure(uint8_t mikrobus_index, uint16_t *measure);
 
 #endif
