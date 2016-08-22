1b396e33d20fb56c574d2796a2d7ba4472859ae4
Francois Berder <fberder@outlook.fr>
2016-08-22 19:46:01 +0100

    test_eve: add load identity test
    Signed-off-by: Francois Berder <fberder@outlook.fr>

--- tests/test_eve_click.c
+++ tests/test_eve_click.c
@@ -413,15 +413,30 @@ static bool test_eve_click_translate_and_set_matrix(void)
         && d == 0 && e == 65536 && f == -6553600;
 }
 
+static bool test_eve_click_load_identity(void)
+{
+    int32_t a, b, c, d, e, f;
+
+    if (eve_click_clear(0, 0, 0) < 0
+    ||  eve_click_load_identity() < 0
+    ||  eve_click_display() < 0
+    ||  eve_click_get_matrix(&a, &b, &c, &d, &e, &f) < 0)
+        return false;
+
+    return a == 65536 && b == 0 && c == 0
+        && d == 0 && e == 65536 && f == 0;
+}
+
 int main(void)
 {
     int ret = -1;
 
-    CREATE_TEST(eve_click, 27)
+    CREATE_TEST(eve_click, 28)
     ADD_TEST_CASE(eve_click, enable_disable);
     ADD_TEST_CASE(eve_click, black_screen_on_enable);
     ADD_TEST_CASE(eve_click, get_matrix);
     ADD_TEST_CASE(eve_click, translate_and_set_matrix);
+    ADD_TEST_CASE(eve_click, load_identity);
     ADD_TEST_CASE(eve_click, spinner);
     ADD_TEST_CASE(eve_click, load_image);
     ADD_TEST_CASE(eve_click, memset_and_memcrc);
