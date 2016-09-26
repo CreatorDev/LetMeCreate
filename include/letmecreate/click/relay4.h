51a7b8f68d4f621ead9aaeb1f4d325f7917019f0
Francois Berder <fberder@outlook.fr>
2016-09-26 20:06:22 +0100

    relay4: refactor wrapper
    Signed-off-by: Francois Berder <fberder@outlook.fr>

--- include/letmecreate/click/relay4.h
+++ include/letmecreate/click/relay4.h
@@ -11,63 +11,62 @@
         products derived from this software without specific prior written permission.
  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES,
  INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
- DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, 
+ DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
  SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
- SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, 
- WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE 
+ SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY,
+ WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE
  USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 ************************************************************************************************************************/
 
 #ifndef __LETMECREATE_CLICK_RELAY4_H__
 #define __LETMECREATE_CLICK_RELAY4_H__
 
-#include <stdbool.h>
+#include <stdint.h>
 
 /**
  * @brief Configure relay4 to be controlled over given pins on cI40 board (those pins should be connected to pins P0-P3
- * on relay4 board). Each pin will be set as GPIO_OUTPUT. Due to Ci40 pins layout it's proposed to use GPIO_83, GPIO_81, 
+ * on relay4 board). Each pin will be set as GPIO_OUTPUT. Due to Ci40 pins layout it's proposed to use GPIO_83, GPIO_81,
  * GPIO_80, GPIO_72 as a default values.
  *
- * @param relay0Pin pin which is connected to relay4 P0 pin
- * @param relay1Pin pin which is connected to relay4 P1 pin
- * @param relay2Pin pin which is connected to relay4 P2 pin
- * @param relay3Pin pin which is connected to relay4 P3 pin
- * @return 0 if success otherwise -1
+ * @param[in] relay_0_pin Index of the pin connected to relay4 P0 pin
+ * @param[in] relay_1_pin Index of the pin connected to relay4 P1 pin
+ * @param[in] relay_2_pin Index of the pin connected to relay4 P2 pin
+ * @param[in] relay_3_pin Index of the pin connected to relay4 P3 pin
+ * @return 0 if successful, -1 otherwise
  */
-uint8_t relay4_enable(uint8_t relay0Pin, uint8_t relay1Pin, uint8_t relay2Pin, uint8_t relay3Pin);
+int relay4_click_enable(uint8_t relay_0_pin, uint8_t relay_1_pin, uint8_t relay_2_pin, uint8_t relay_3_pin);
 
 /**
- * @brief Releases usage of GPIO pins reserved through call to relay4_enable or relay4_enable_defaults.
+ * @brief Releases usage of GPIO pins reserved through call to relay4_enable.
  *
- * @return 0 if success otherwise -1
+ * @return 0 if successful, -1 otherwise
  */
-uint8_t relay4_disable();
+int relay4_click_disable(void);
 
 /**
  * @brief Changes state of one of 4 relay switch. Input parameter is a index number not a GPIO pin ID.
  *
- * @param pinIndex Index of the relay which state will be set.
- * @param on Set to true if relay should be turned on.
- * @return 0 if success otherwise -1
+ * @param[in] pin_index Index of the relay which state will be set.
+ * @param[in] state Set to 1 if relay should be turned on, 0 otherwise
+ * @return 0 if successful, -1 otherwise
  */
-uint8_t relay4_set_state(uint8_t pinIndex, bool on);
+int relay4_click_set_state(uint8_t pin_index, uint8_t state);
 
 /**
  * @brief Returns state of one of 4 relay switch. Input parameter is a index number not a GPIO pin ID.
  *
- * @param pinIndex Index of the relay which state will be returned.
- * @param[out] state Current state of switch.
- * @return 0 if success otherwise -1
+ * @param[in] pin_index Index of the relay which state will be returned.
+ * @param[out] state Current state of switch (0: low, 1: high)
+ * @return 0 if successful, -1 otherwise
  */
-uint8_t relay4_get_state(uint8_t pinIndex, bool* state);
+int relay4_click_get_state(uint8_t pin_index, uint8_t* state);
 
 /**
  * @brief Changes state of one of 4 relay switch to opposite.
  *
- * @param pinIndex Index of the relay which state will be changed.
- * @param[out] newState State of switch after toggle operation.
- * @return 0 if success otherwise -1
+ * @param[in] pin_index Index of the relay which state will be changed.
+ * @return 0 if successful, -1 otherwise
  */
-uint8_t relay4_toggle(uint8_t pinIndex, bool* newState);
+int relay4_click_toggle(uint8_t pin_index);
 
 #endif /* __LETMECREATE_CLICK_RELAY4_H__ */
--- src/click/relay4.c
+++ src/click/relay4.c
@@ -11,94 +11,103 @@
         products derived from this software without specific prior written permission.
  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES,
  INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
- DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, 
+ DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
  SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
- SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, 
- WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE 
+ SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY,
+ WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE
  USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 ************************************************************************************************************************/
 
-#include <stdlib.h>
 #include <stdbool.h>
-#include <letmecreate/letmecreate.h>
 #include <stdio.h>
+#include "letmecreate/click/relay4.h"
+#include "letmecreate/core/gpio.h"
 
-static uint8_t relay4_pins[4] = {0, 0, 0, 0};
+#define RELAY4_PIN_COUNT        (4)
 
+static uint8_t relay4_pins[RELAY4_PIN_COUNT] = {};
 
-uint8_t relay4_enable(uint8_t relay0Pin, uint8_t relay1Pin, uint8_t relay2Pin, uint8_t relay3Pin) {
-    relay4_pins[0] = relay0Pin;
-    relay4_pins[1] = relay1Pin;
-    relay4_pins[2] = relay2Pin;
-    relay4_pins[3] = relay3Pin;
+static bool check_valid_pin_index(uint8_t pin_index)
+{
+    return pin_index < RELAY4_PIN_COUNT && relay4_pins[pin_index] != 0;
+}
 
-    if (gpio_init(relay0Pin) < 0
-    ||  gpio_init(relay1Pin) < 0
-    ||  gpio_init(relay2Pin) < 0
-    ||  gpio_init(relay3Pin) < 0
-    ||  gpio_set_direction(relay0Pin, GPIO_OUTPUT) < 0
-    ||  gpio_set_direction(relay1Pin, GPIO_OUTPUT) < 0
-    ||  gpio_set_direction(relay2Pin, GPIO_OUTPUT) < 0
-    ||  gpio_set_direction(relay3Pin, GPIO_OUTPUT) < 0) {
-        fprintf(stderr, "relay4: Failed to configure pins\n");
-        return -1;
+int relay4_click_enable(uint8_t relay_0_pin, uint8_t relay_1_pin, uint8_t relay_2_pin, uint8_t relay_3_pin)
+{
+    uint8_t i;
+
+    relay4_pins[0] = relay_0_pin;
+    relay4_pins[1] = relay_1_pin;
+    relay4_pins[2] = relay_2_pin;
+    relay4_pins[3] = relay_3_pin;
+
+    for (i = 0; i < RELAY4_PIN_COUNT; ++i) {
+        if (gpio_init(relay4_pins[i]) < 0
+        ||  gpio_set_direction(relay4_pins[i], GPIO_OUTPUT) < 0) {
+            fprintf(stderr, "relay4: Failed to configure relay %u pin.\n", i);
+            return -1;
+        }
     }
 
     return 0;
 }
 
-uint8_t relay4_disable() {
-    uint8_t result = 0;
-    int t;
-    for(t = 0; t < 4; t++) {
-        if (relay4_pins[t] > 0) {
-            if (gpio_release(relay4_pins[t]) < 0) {
-                result = -1;
+int relay4_click_disable(void)
+{
+    int ret = 0;
+    uint8_t i;
+
+    for(i = 0; i < RELAY4_PIN_COUNT; ++i) {
+        if (relay4_pins[i] > 0) {
+            if (gpio_release(relay4_pins[i]) < 0) {
+                fprintf(stderr, "relay4: Failed to release relay %u pin.\n", i);
+                ret = -1;
             }
-            relay4_pins[t] = 0;
+            relay4_pins[i] = 0;
         }
     }
-    return result;
+
+    return ret;
 }
 
-uint8_t relay4_set_state(uint8_t pinIndex, bool on) {
-    if ((pinIndex > 3) || (relay4_pins[pinIndex] == 0)) {
-        fprintf(stderr, "relay4: Wrong pin Index, or pin is not initialized through call to relay4_enable.\n");
+int relay4_click_set_state(uint8_t pin_index, uint8_t state)
+{
+    if (!check_valid_pin_index(pin_index)) {
+        fprintf(stderr, "relay4: Wrong pin index, or pin is not initialized through call to relay4_enable.\n");
         return -1;
-
     }
-    return gpio_set_value(relay4_pins[pinIndex], on);
+
+    return gpio_set_value(relay4_pins[pin_index], state);
 }
 
-uint8_t relay4_get_state(uint8_t pinIndex, bool* state) {
-    if ((pinIndex > 3) || (relay4_pins[pinIndex] == 0)) {
-        fprintf(stderr, "relay4: Wrong pin Index, or pin is not initialized through call to relay4_enable.\n");
+int relay4_click_get_state(uint8_t pin_index, uint8_t* state)
+{
+    if (!check_valid_pin_index(pin_index)) {
+        fprintf(stderr, "relay4: Wrong pin index, or pin is not initialized through call to relay4_enable.\n");
         return -1;
+    }
 
+    if (state == NULL) {
+        fprintf(stderr, "relay4: Cannot store state using null pointer.\n");
+        return -1;
     }
-    uint8_t intState;
-    uint8_t result = gpio_get_value(relay4_pins[pinIndex], &intState);
-    if (state != NULL) {
-        *state = (bool)intState;
-    }
-    return result;
+
+    return gpio_get_value(relay4_pins[pin_index], state);
 }
 
-uint8_t relay4_toggle(uint8_t pinIndex, bool* newState) {
-    if ((pinIndex > 3) || (relay4_pins[pinIndex] == 0)) {
-        fprintf(stderr, "relay4: Wrong pin Index, or pin is not initialized through call to relay4_enable.\n");
-        return -1;
+int relay4_click_toggle(uint8_t pin_index)
+{
+    uint8_t state = 0;
 
-    }
-    uint8_t intState;
-    if (gpio_get_value(relay4_pins[pinIndex], &intState) < 0) {
-        fprintf(stderr, "relay4: Failed to get gpio value\n");
+    if (!check_valid_pin_index(pin_index)) {
+        fprintf(stderr, "relay4: Wrong pin index, or pin is not initialized through call to relay4_enable.\n");
         return -1;
     }
-    //note: new state is negation of current state
-    bool state = intState == 0 ? true : false;
-    if (newState != NULL) {
-        *newState = state;
+
+    if (gpio_get_value(relay4_pins[pin_index], &state) < 0) {
+        fprintf(stderr, "relay4: Failed to get gpio value.\n");
+        return -1;
     }
-    return gpio_set_value(relay4_pins[pinIndex], state);
+
+    return gpio_set_value(relay4_pins[pin_index], !state);
 }
