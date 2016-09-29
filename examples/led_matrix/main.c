8320e28f03b25c6b7f92ebea5d1805f6c3d1c761
Francois Berder <fberder@outlook.fr>
2016-09-29 19:44:35 +0100

    led_matrix: add description in example
    Signed-off-by: Francois Berder <fberder@outlook.fr>

--- examples/led_matrix/main.c
+++ examples/led_matrix/main.c
@@ -1,3 +1,13 @@
+/**
+ * This example shows how to use the 8x8R Click wrapper of the LetMeCreate.
+ *
+ * It gradually turns on all the LED's of the led matrix (8x8R Click) from the
+ * bottom-right corner to the top-left corner.
+ *
+ * The 8x8R Click must be inserted in Mikrobus 1 before running this program.
+ */
+
+
 #include <linux/spi/spidev.h>
 #include <letmecreate/letmecreate.h>
 #include "examples/common.h"
