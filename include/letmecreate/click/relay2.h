e73c61d24e92e09cbe6008034fa378faba07ec94
Francois Berder <fberder@outlook.fr>
2016-10-28 22:38:18 +0100

    relay2: refactor wrapper and add read state
    Signed-off-by: Francois Berder <fberder@outlook.fr>

--- include/letmecreate/click/relay2.h
+++ include/letmecreate/click/relay2.h
@@ -13,36 +13,48 @@
 #include <stdint.h>
 #include <letmecreate/click/export.h>
 
-/**
- * @brief Enable relay 1 on Relay2 Click.
- *
- * @param[in] mikrobus_index Index of the mikrobus (see #MIKROBUS_INDEX)
- * @return 0 if successful, -1 otherwise
- */
-int LETMECREATE_CLICK_EXPORT relay2_click_enable_relay_1(uint8_t mikrobus_index);
+enum RELAY2_CLICK_RELAY {
+    RELAY2_CLICK_RELAY_1,
+    RELAY2_CLICK_RELAY_2,
+    RELAY2_CLICK_RELAY_COUNT
+};
 
 /**
- * @brief Disable relay 1 on Relay2 Click.
+ * @brief Enable a relay of Relay2 Click.
  *
  * @param[in] mikrobus_index Index of the mikrobus (see #MIKROBUS_INDEX)
+ * @param[in] relay Index of the relay to enable (see #RELAY2_CLICK_RELAY)
  * @return 0 if successful, -1 otherwise
  */
-int LETMECREATE_CLICK_EXPORT relay2_click_disable_relay_1(uint8_t mikrobus_index);
+int LETMECREATE_CLICK_EXPORT relay2_click_enable_relay(uint8_t mikrobus_index, uint8_t relay);
 
 /**
- * @brief Enable relay 2 on Relay2 Click.
+ * @brief Disable a relay of Relay2 Click.
  *
  * @param[in] mikrobus_index Index of the mikrobus (see #MIKROBUS_INDEX)
+ * @param[in] relay Index of the realy to disable (see #RELAY2_CLICK_RELAY)
  * @return 0 if successful, -1 otherwise
  */
-int LETMECREATE_CLICK_EXPORT relay2_click_enable_relay_2(uint8_t mikrobus_index);
+int LETMECREATE_CLICK_EXPORT relay2_click_disable_relay(uint8_t mikrobus_index, uint8_t relay);
 
 /**
- * @brief Disable relay 2 on Relay2 Click.
+ * @brief Set state a relay of Relay2 Click.
  *
  * @param[in] mikrobus_index Index of the mikrobus (see #MIKROBUS_INDEX)
+ * @param[in] relay Index of the realy to disable (see #RELAY2_CLICK_RELAY)
+ * @param[in] state 0 for disabling a relay, any other value for enabling a relay
  * @return 0 if successful, -1 otherwise
  */
-int LETMECREATE_CLICK_EXPORT relay2_click_disable_relay_2(uint8_t mikrobus_index);
+int relay2_click_set_relay_state(uint8_t mikrobus_index, uint8_t relay, uint8_t state);
+
+/**
+ * @brief Get current state (on/off) of a relay.
+ *
+ * @param[in] mikrobus_index Index of the mikrobus (see #MIKROBUS_INDEX)
+ * @param[in] relay Index of the realy to disable (see #RELAY2_CLICK_RELAY)
+ * @param[out] state 0 if off, 1 if on
+ * @return 0 if successful, -1 otherwise
+ */
+int LETMECREATE_CLICK_EXPORT relay2_click_get_relay_state(uint8_t mikrobus_index, uint8_t relay, uint8_t *state);
 
 #endif
--- src/click/relay2.c
+++ src/click/relay2.c
@@ -4,86 +4,79 @@
 #include <letmecreate/click/relay2.h>
 
 
-int relay2_click_enable_relay_1(uint8_t mikrobus_index)
+static int find_gpio(uint8_t mikrobus_index, uint8_t relay)
 {
-    switch (mikrobus_index) {
-    case MIKROBUS_1:
-        if (!gpio_init(MIKROBUS_1_PWM)
-        &&  !gpio_set_direction(MIKROBUS_1_PWM, GPIO_OUTPUT)
-        &&  !gpio_set_value(MIKROBUS_1_PWM, 1))
-            return 0;
-    case MIKROBUS_2:
-        if (!gpio_init(MIKROBUS_2_PWM)
-        &&  !gpio_set_direction(MIKROBUS_2_PWM, GPIO_OUTPUT)
-        &&  !gpio_set_value(MIKROBUS_2_PWM, 1))
-            return 0;
-        default:
-            fprintf(stderr, "relay2: Invalid mikrobus index.\n");
-            return -1;
+    uint8_t gpio_pin = 0;
+
+    if (mikrobus_index != MIKROBUS_1 && mikrobus_index != MIKROBUS_2) {
+        fprintf(stderr, "relay2: Invalid mikrobus index.\n");
+        return -1;
     }
 
-    return -1;
+    if (relay >= RELAY2_CLICK_RELAY_COUNT) {
+        fprintf(stderr, "relay2: Invalid relay index.\n");
+        return -1;
+    }
+
+    if (mikrobus_index == MIKROBUS_1) {
+        if (relay == RELAY2_CLICK_RELAY_1)
+            gpio_pin = MIKROBUS_1_PWM;
+        else
+            gpio_pin = MIKROBUS_1_AN;
+    } else { /* mikrobus_index == MIKROBUS_2 */
+        if (relay == RELAY2_CLICK_RELAY_1)
+            gpio_pin = MIKROBUS_2_PWM;
+        else
+            gpio_pin = MIKROBUS_2_AN;
+    }
+
+    return gpio_pin;
 }
 
-int relay2_click_disable_relay_1(uint8_t mikrobus_index)
+int relay2_click_enable_relay(uint8_t mikrobus_index, uint8_t relay)
 {
-    switch (mikrobus_index) {
-    case MIKROBUS_1:
-        if (!gpio_init(MIKROBUS_1_PWM)
-        &&  !gpio_set_direction(MIKROBUS_1_PWM, GPIO_OUTPUT)
-        &&  !gpio_set_value(MIKROBUS_1_PWM, 0))
-            return 0;
-    case MIKROBUS_2:
-        if (!gpio_init(MIKROBUS_2_PWM)
-        &&  !gpio_set_direction(MIKROBUS_2_PWM, GPIO_OUTPUT)
-        &&  !gpio_set_value(MIKROBUS_2_PWM, 0))
-            return 0;
-        default:
-            fprintf(stderr, "relay2: Invalid mikrobus index.\n");
-            return -1;
-    }
-
-    return -1;
+    return relay2_click_set_relay_state(mikrobus_index, relay, 1);
 }
 
-int relay2_click_enable_relay_2(uint8_t mikrobus_index)
+int relay2_click_disable_relay(uint8_t mikrobus_index, uint8_t relay)
 {
-    switch (mikrobus_index) {
-    case MIKROBUS_1:
-        if (!gpio_init(MIKROBUS_1_AN)
-        &&  !gpio_set_direction(MIKROBUS_1_AN, GPIO_OUTPUT)
-        &&  !gpio_set_value(MIKROBUS_1_AN, 1))
-            return 0;
-    case MIKROBUS_2:
-        if (!gpio_init(MIKROBUS_2_AN)
-        &&  !gpio_set_direction(MIKROBUS_2_AN, GPIO_OUTPUT)
-        &&  !gpio_set_value(MIKROBUS_2_AN, 1))
-            return 0;
-        default:
-            fprintf(stderr, "relay2: Invalid mikrobus index.\n");
-            return -1;
-    }
-
-    return -1;
+    return relay2_click_set_relay_state(mikrobus_index, relay, 0);
 }
 
-int relay2_click_disable_relay_2(uint8_t mikrobus_index)
+int relay2_click_set_relay_state(uint8_t mikrobus_index, uint8_t relay, uint8_t state)
 {
-    switch (mikrobus_index) {
-    case MIKROBUS_1:
-        if (!gpio_init(MIKROBUS_1_AN)
-        &&  !gpio_set_direction(MIKROBUS_1_AN, GPIO_OUTPUT)
-        &&  !gpio_set_value(MIKROBUS_1_AN, 0))
-            return 0;
-    case MIKROBUS_2:
-        if (!gpio_init(MIKROBUS_2_AN)
-        &&  !gpio_set_direction(MIKROBUS_2_AN, GPIO_OUTPUT)
-        &&  !gpio_set_value(MIKROBUS_2_AN, 0))
-            return 0;
-        default:
-            fprintf(stderr, "relay2: Invalid mikrobus index.\n");
-            return -1;
+    uint8_t gpio_pin = 0;
+    int ret = 0;
+
+    ret = find_gpio(mikrobus_index, relay);
+    if (ret < 0)
+        return -1;
+
+    gpio_pin = (uint8_t)ret;
+
+    if (gpio_init(gpio_pin) < 0
+    ||  gpio_set_direction(gpio_pin, GPIO_OUTPUT) < 0
+    ||  gpio_set_value(gpio_pin, !!state) < 0)
+        return -1;
+
+    return 0;
+}
+
+int LETMECREATE_CLICK_EXPORT relay2_click_get_relay_state(uint8_t mikrobus_index, uint8_t relay, uint8_t *state)
+{
+    uint8_t gpio_pin = 0;
+    int ret = 0;
+
+    if (state == NULL) {
+        fprintf(stderr, "relay2: Cannot store state using null pointer.\n");
+        return -1;
     }
 
-    return -1;
+    ret = find_gpio(mikrobus_index, relay);
+    if (ret < 0)
+        return -1;
+
+    gpio_pin = (uint8_t)ret;
+
+    return gpio_get_value(gpio_pin, state);
 }
