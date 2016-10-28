b65cacd54fe64d00f5c953e6627865d50f3b6944
Francois Berder <fberder@outlook.fr>
2016-10-28 22:38:33 +0100

    relay: refactor and add get/set state for relay 1
    Signed-off-by: Francois Berder <fberder@outlook.fr>

--- src/click/relay.c
+++ src/click/relay.c
@@ -4,44 +4,59 @@
 #include <letmecreate/core/gpio.h>
 
 
-int relay_click_enable_relay_1(uint8_t mikrobus_index)
+static uint8_t find_gpio(uint8_t mikrobus_index)
 {
+    uint8_t gpio_pin = 0;
+
     switch (mikrobus_index) {
     case MIKROBUS_1:
-        if (!gpio_init(MIKROBUS_1_PWM)
-        &&  !gpio_set_direction(MIKROBUS_1_PWM, GPIO_OUTPUT)
-        &&  !gpio_set_value(MIKROBUS_1_PWM, 1))
-            return 0;
+        gpio_pin = MIKROBUS_1_PWM;
+        break;
     case MIKROBUS_2:
-        if (!gpio_init(MIKROBUS_2_PWM)
-        &&  !gpio_set_direction(MIKROBUS_2_PWM, GPIO_OUTPUT)
-        &&  !gpio_set_value(MIKROBUS_2_PWM, 1))
-            return 0;
-        default:
-            fprintf(stderr, "relay: Invalid mikrobus index.\n");
-            return -1;
+        gpio_pin = MIKROBUS_2_PWM;
+        break;
+    default:
+        fprintf(stderr, "relay: Invalid mikrobus index.\n");
+        break;
     }
 
-    return -1;
+    return gpio_pin;
+}
+
+int relay_click_enable_relay_1(uint8_t mikrobus_index)
+{
+    return relay_click_set_relay_1_state(mikrobus_index, 1);
 }
 
 int relay_click_disable_relay_1(uint8_t mikrobus_index)
 {
-    switch (mikrobus_index) {
-    case MIKROBUS_1:
-        if (!gpio_init(MIKROBUS_1_PWM)
-        &&  !gpio_set_direction(MIKROBUS_1_PWM, GPIO_OUTPUT)
-        &&  !gpio_set_value(MIKROBUS_1_PWM, 0))
-            return 0;
-    case MIKROBUS_2:
-        if (!gpio_init(MIKROBUS_2_PWM)
-        &&  !gpio_set_direction(MIKROBUS_2_PWM, GPIO_OUTPUT)
-        &&  !gpio_set_value(MIKROBUS_2_PWM, 0))
-            return 0;
-        default:
-            fprintf(stderr, "relay: Invalid mikrobus index.\n");
-            return -1;
+    return relay_click_set_relay_1_state(mikrobus_index, 0);
+}
+
+int relay_click_set_relay_1_state(uint8_t mikrobus_index, uint8_t state)
+{
+    uint8_t gpio_pin = find_gpio(mikrobus_index);
+    if (gpio_pin == 0)
+        return -1;
+
+    if (gpio_init(gpio_pin) < 0
+    ||  gpio_set_direction(gpio_pin, GPIO_OUTPUT) < 0
+    ||  gpio_set_value(gpio_pin, !!state) < 0)
+        return -1;
+
+    return 0;
+}
+
+int relay_click_get_relay_1_state(uint8_t mikrobus_index, uint8_t *state)
+{
+    uint8_t gpio_pin = find_gpio(mikrobus_index);
+    if (gpio_pin == 0)
+        return -1;
+
+    if (state == NULL) {
+        fprintf(stderr, "relay: \n");
+        return -1;
     }
 
-    return -1;
+    return gpio_get_value(gpio_pin, state);
 }
