414773e982aa3035da0f91291e3f6d8d808aa9d2
Francois Berder <fberder@outlook.fr>
2016-11-09 19:40:18 +0000

    alphanum: add variable names in header
    Signed-off-by: Francois Berder <fberder@outlook.fr>

--- include/letmecreate/click/alphanum.h
+++ include/letmecreate/click/alphanum.h
@@ -8,20 +8,20 @@
 #define ALPHANUM_SWITCH_INTERVAL 8
 
 /* Init the alphanum clicker */
-int LETMECREATE_CLICK_EXPORT alphanum_click_init(uint8_t);
+int LETMECREATE_CLICK_EXPORT alphanum_click_init(uint8_t mikrobus_index);
 
 /* Converts a char into a 14 segment display value */
 int LETMECREATE_CLICK_EXPORT alphanum_click_get_char(char c, uint16_t *value);
 
 /* Write the two chars to */
-int LETMECREATE_CLICK_EXPORT alphanum_click_write(char, char);
+int LETMECREATE_CLICK_EXPORT alphanum_click_write(char a, char b);
 
 /* Raw write  */
-int LETMECREATE_CLICK_EXPORT alphanum_click_raw_write(uint16_t, uint16_t);
+int LETMECREATE_CLICK_EXPORT alphanum_click_raw_write(uint16_t a, uint16_t b);
 
 /* This peridically switch between segment a and b to keep the illusion of a
  * simultanous display of both values
  */
-void LETMECREATE_CLICK_EXPORT alphanum_click_switch_cycles(int);
+void LETMECREATE_CLICK_EXPORT alphanum_click_switch_cycles(int iteration_count);
 
 #endif
