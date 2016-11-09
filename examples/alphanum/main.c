a7237c9caff1dae050ae9b4bfdaeddc093ef7f75
Francois Berder <fberder@outlook.fr>
2016-11-09 19:36:30 +0000

    alphanum: change tabs to spaces for indentation
    Signed-off-by: Francois Berder <fberder@outlook.fr>

--- examples/alphanum/main.c
+++ examples/alphanum/main.c
@@ -18,31 +18,31 @@
 int
 main(int argc, char *argv[])
 {
-	int ret = 0;
-	char str[] = "Hi"; /* String to print */
+    int ret = 0;
+    char str[] = "Hi"; /* String to print */
 
-	/* SPI int */
-	if ((ret = spi_init()) != 0) {
-		printf("Error spi init failed with %i\n", ret);
-		return ret;
-	}
+    /* SPI int */
+    if ((ret = spi_init()) != 0) {
+        printf("Error spi init failed with %i\n", ret);
+        return ret;
+    }
 
-	/* Initialize the alphanum clicker */
-	if (alphanum_init(MIKROBUS_2) != 0) {
-		printf("Error: Clicker cannot be initiliazed\n");
-		return 1;
-	}
+    /* Initialize the alphanum clicker */
+    if (alphanum_init(MIKROBUS_2) != 0) {
+        printf("Error: Clicker cannot be initiliazed\n");
+        return 1;
+    }
 
-	printf("Writing: %s\n", str);
-	if (alphanum_write(str[0], str[1]) != 0) {
-		printf("Error: Cannot write to alphanum clicker\n");
-		return 1;
-	}
+    printf("Writing: %s\n", str);
+    if (alphanum_write(str[0], str[1]) != 0) {
+        printf("Error: Cannot write to alphanum clicker\n");
+        return 1;
+    }
 
-	/* This alternately switches on the output of either the two shift registers
-	 * to appear to print two characters at the same time.
-	 */
-	alphanum_switch_cycles(0);
+    /* This alternately switches on the output of either the two shift registers
+     * to appear to print two characters at the same time.
+     */
+    alphanum_switch_cycles(0);
 
-	return 0;
+    return 0;
 }
