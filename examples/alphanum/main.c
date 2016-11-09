85fa6d413bb6d29ca562151766aa4890e894e843
Francois Berder <fberder@outlook.fr>
2016-11-09 19:41:01 +0000

    alphanum: simplify example
    Signed-off-by: Francois Berder <fberder@outlook.fr>

--- examples/alphanum/main.c
+++ examples/alphanum/main.c
@@ -1,33 +1,17 @@
 #include <stdio.h>
 #include <letmecreate/letmecreate.h>
 
-/*
- * Example to write the characters on the alphanum clicker.
- */
+
 int main(void)
 {
-    int ret = 0;
-    char str[] = "Hi"; /* String to print */
-
-    /* SPI int */
-    if ((ret = spi_init()) != 0) {
-        printf("Error spi init failed with %i\n", ret);
-        return ret;
-    }
-
+    spi_init();
     spi_select_bus(MIKROBUS_1);
 
-    /* Initialize the alphanum clicker */
-    if (alphanum_click_init(MIKROBUS_1) != 0) {
-        printf("Error: Clicker cannot be initiliazed\n");
+    if (alphanum_click_init(MIKROBUS_1) < 0)
         return 1;
-    }
 
-    printf("Writing: %s\n", str);
-    if (alphanum_click_write(str[0], str[1]) != 0) {
-        printf("Error: Cannot write to alphanum clicker\n");
+    if (alphanum_click_write('C', 'i') < 0)
         return 1;
-    }
 
     /* This alternately switches on the output of either the two shift registers
      * to appear to print two characters at the same time.
