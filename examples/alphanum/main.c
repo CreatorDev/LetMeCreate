4b2ac8dc29ab9b585dfdbbdd1b788be94887c4ac
Francois Berder <fberder@outlook.fr>
2016-11-09 19:41:28 +0000

    alphanum: add signal handler in example
    Signed-off-by: Francois Berder <fberder@outlook.fr>

--- examples/alphanum/main.c
+++ examples/alphanum/main.c
@@ -1,9 +1,28 @@
+#include <signal.h>
+#include <stdbool.h>
 #include <stdio.h>
 #include <letmecreate/letmecreate.h>
 #include "examples/common.h"
 
+static volatile bool running = true;
+
+static void exit_program(int __attribute__ ((unused))signo)
+{
+    running = false;
+}
+
 int main(void)
 {
+    /* Set signal handler to exit program when Ctrl+c is pressed */
+    struct sigaction action = {
+        .sa_handler = exit_program,
+        .sa_flags = 0
+    };
+    sigemptyset(&action.sa_mask);
+    sigaction (SIGINT, &action, NULL);
+
+    printf("Press Ctrl+c to exit program.\n");
+
     spi_init();
     spi_select_bus(MIKROBUS_1);
 
@@ -16,13 +35,14 @@ int main(void)
     /* This alternately switches on the output of either the two shift registers
      * to appear to print two characters at the same time.
      */
-    while (1) {
+    while (running) {
         alphanum_click_select_left_display();
         sleep_ms(5);
         alphanum_click_select_right_display();
         sleep_ms(5);
     }
 
+    spi_release();
 
     return 0;
 }
