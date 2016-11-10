904cf5b7d0d2726aa4ae39168098d4a31e6cb97e
Francois Berder <fberder@outlook.fr>
2016-11-10 19:27:27 +0000

    oled: simplify example
    Signed-off-by: Francois Berder <fberder@outlook.fr>

--- examples/oled/main.c
+++ examples/oled/main.c
@@ -4,20 +4,10 @@
 /*
  * Example how to write a text to the OLED display.
  */
-int
-main(int argc, char *argv[])
+int main(void)
 {
-    int ret = 0;
-
-    if ((ret = i2c_init()) != 0) {
-        printf("Error: Cannot initialize i2c.\n");
-        return ret;
-    }
-
-    if (oled_init(MIKROBUS_2) < 0) {
-        printf("Error: Cannot initialize oled clicker.\n");
-        return 1;
-    }
+    i2c_init();
+    oled_init(MIKROBUS_2);
     oled_write_text("Hello   Creator!");
 
     return 0;
