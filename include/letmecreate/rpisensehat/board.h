1d3f5a6349226c894c4e38311778758c7893056c
Francois Berder <fberder@outlook.fr>
2016-11-15 07:56:11 +0000

    rpisensehat: lsdm9ds1: add wrapper
    Signed-off-by: Francois Berder <fberder@outlook.fr>

--- include/letmecreate/rpisensehat/board.h
+++ include/letmecreate/rpisensehat/board.h
@@ -19,6 +19,12 @@ int LETMECREATE_RPISENSEHAT_EXPORT rpisensehat_get_humidity(float *humidity);
 
 int LETMECREATE_RPISENSEHAT_EXPORT rpisensehat_get_pressure(float *pressure);
 
+int LETMECREATE_RPISENSEHAT_EXPORT rpisensehat_get_accelerometer_measure(float *accelX, float *accelY, float *accelZ);
+
+int LETMECREATE_RPISENSEHAT_EXPORT rpisensehat_get_gyroscope_measure(float *gyroX, float *gyroY, float *gyroZ);
+
+int LETMECREATE_RPISENSEHAT_EXPORT rpisensehat_get_magnetometer_measure(float *magX, float *magY, float *magZ);
+
 int LETMECREATE_RPISENSEHAT_EXPORT rpisensehat_release(void);
 
 #endif
--- src/rpisensehat/board.c
+++ src/rpisensehat/board.c
@@ -1,8 +1,9 @@
 #include <time.h>
 #include <letmecreate/core/gpio.h>
 #include <letmecreate/rpisensehat/board.h>
-#include <letmecreate/rpisensehat/lps25h.h>
 #include <letmecreate/rpisensehat/hts221.h>
+#include <letmecreate/rpisensehat/lps25h.h>
+#include <letmecreate/rpisensehat/lsm9ds1.h>
 
 
 static void sleep_50ms(void)
@@ -43,8 +44,9 @@ int rpisensehat_init(void)
         return -1;
 
     if (hts221_enable() < 0     /* Enable humidity sensor */
-    ||  lps25h_enable() < 0)    /* Enable pressure sensor */
-        return -1;
+    ||  lps25h_enable() < 0     /* Enable pressure sensor */
+    ||  lsm9ds1_enable() < 0)   /* Enable accel/gyro/mag sensor */
+     return -1;
 
     return 0;
 }
@@ -64,10 +66,26 @@ int rpisensehat_get_pressure(float *pressure)
     return lps25h_get_pressure_measure(pressure);
 }
 
+int rpisensehat_get_accelerometer_measure(float *accelX, float *accelY, float *accelZ)
+{
+    return lsm9ds1_get_accelerometer_measure(accelX, accelY, accelZ);
+}
+
+int rpisensehat_get_gyroscope_measure(float *gyroX, float *gyroY, float *gyroZ)
+{
+    return lsm9ds1_get_gyroscope_measure(gyroX, gyroY, gyroZ);
+}
+
+int rpisensehat_get_magnetometer_measure(float *magX, float *magY, float *magZ)
+{
+    return lsm9ds1_get_magnetometer_measure(magX, magY, magZ);
+}
+
 int rpisensehat_release(void)
 {
     if (hts221_disable() < 0    /* Disable humidity sensor */
-    ||  lps25h_disable() < 0)   /* Disable pressure sensor */
+    ||  lps25h_disable() < 0    /* Disable pressure sensor */
+    ||  lsm9ds1_disable() < 0)  /* Disable accel/gyro/mag sensor */
         return -1;
 
     return 0;
