938fd497205c602dc620d1a1a61fb438d0bae593
Francois Berder <fberder@outlook.fr>
2016-11-15 07:55:18 +0000

    rpisensehat: add basic wrapper
    Signed-off-by: Francois Berder <fberder@outlook.fr>

---
+++ include/letmecreate/rpisensehat/board.h
@@ -0,0 +1,16 @@
+/**
+ * @file board.h
+ * @author Francois Berder
+ * @date 2016
+ * @copyright 3-clause BSD
+ */
+
+
+#ifndef __LETMECREATE_RPISENSEHAT_BOARD_H__
+#define __LETMECREATE_RPISENSEHAT_BOARD_H__
+
+#include <letmecreate/rpisensehat/export.h>
+
+int LETMECREATE_RPISENSEHAT_EXPORT rpisensehat_init(void);
+
+#endif
---
+++ src/rpisensehat/board.c
@@ -0,0 +1,8 @@
+#include <letmecreate/rpisensehat/board.h>
+
+
+int rpisensehat_init(void)
+{
+    /* Not yet implemented */
+    return -1;
+}
