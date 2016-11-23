e626a863255d89edf2af7c3a3bd5588f77b4e303
Francois Berder <fberder@outlook.fr>
2016-11-23 23:03:44 +0000

    rtc: add documentation for date structure
    Signed-off-by: Francois Berder <fberder@outlook.fr>

--- include/letmecreate/click/rtc.h
+++ include/letmecreate/click/rtc.h
@@ -38,20 +38,21 @@ enum MONTH {
     DECEMBER
 };
 
+/** Structure to represent a date */
 struct date {
     uint16_t year;          /**< Must be in range year_offset..year_offset+3 */
     unsigned int month:4;   /**< Must be in range 0..11 */
-    unsigned int day:5;
+    unsigned int day:5;     /**< Must be in range 1..31 */
     unsigned int weekday:4; /**< Must be in range 0..6 */
-    unsigned int hour:5;
-    unsigned int minute:6;
-    unsigned int second:6;
+    unsigned int hour:5;    /**< Must be in range 0..23 */
+    unsigned int minute:6;  /**< Must be in range 0..59 */
+    unsigned int second:6;  /**< Must be in range 0..59 */
 };
 
 /**
  * @brief Initialise the RTC Click.
  *
- * @param[in] year_offset RTC Click store year on 2 bits only. year_offset is
+ * @param[in] year_offset RTC Click stores year on 2 bits only. year_offset is
  * used to compute year as year_offset + 2bits from RTC click.
  * @return 0 if successful, -1 otherwise
  */
