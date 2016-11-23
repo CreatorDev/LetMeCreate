026f507aeeefba1547e04b7d9d820046e56127ef
Francois Berder <fberder@outlook.fr>
2016-11-23 23:04:49 +0000

    rpisensehat: hts221: add documentation
    Signed-off-by: Francois Berder <fberder@outlook.fr>

--- include/letmecreate/rpisensehat/hts221.h
+++ include/letmecreate/rpisensehat/hts221.h
@@ -9,9 +9,46 @@
 #ifndef __LETMECREATE_RPISENSEHAT_HTS221_H__
 #define __LETMECREATE_RPISENSEHAT_HTS221_H__
 
+/**
+ * @brief Enable and configure sensor.
+ *
+ * Heater is disabled, interrupt signel is disabled.
+ * Calibration data for temperature and humidity are read.
+ *
+ * This function must be called before attempting to get temperature or humidity
+ * from the sensor.
+ *
+ * @return 0 if successful, -1 otherwise
+ */
 int hts221_enable(void);
+
+/**
+ * @brief Read temperature measure.
+ *
+ * Calibration data are used to compute the temperature.
+ *
+ * @param[out] temperature in degrees celsius (must not be null)
+ * @return 0 if successful, -1 otherwise
+ */
 int hts221_get_temperature_measure(float *temperature);
+
+/**
+ * @brief Read humidity measure.
+ *
+ * Calibration data are used to compute the humidity.
+ *
+ * @param[out] humidity Percentage of humidity in range 0..100 (must not be null)
+ * @return 0 if successful, -1 otherwise
+ */
 int hts221_get_humidity_measure(float *humidity);
+
+/**
+ * @brief Disable sensor.
+ *
+ * Power down sensor, disable heater.
+ *
+ * @return 0 if successful, -1 otherwise
+ */
 int hts221_disable(void);
 
 #endif
