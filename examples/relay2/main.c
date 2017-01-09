77962bc60dbb48c5ff03416b39334ae15172a962
Francois Berder <fberder@outlook.fr>
2017-01-09 18:32:31 +0000

    relay2: add description in example
    Signed-off-by: Francois Berder <fberder@outlook.fr>

--- examples/relay2/main.c
+++ examples/relay2/main.c
@@ -1,3 +1,16 @@
+/**
+ * This examples shows how to use the Relay2 Click wrapper of the LetMeCreate
+ *library.
+ *
+ * It switches on/off relays in the following order:
+ *    - relay 1 off, relay 2 off
+ *    - relay 1 on, relay 2 off
+ *    - relay 1 off, relay 2 on
+ *    - relay 1 on, relay 2 on
+ *
+ * Relay2 Click must be inserted in mikrobus 1 before running this program.
+ */
+
 #include <unistd.h>
 #include <letmecreate/letmecreate.h>
 
