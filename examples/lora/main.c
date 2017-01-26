f3915e86fdd4b528804219fb0c9acd208c494df6
Francois Berder <francois.berder@imgtec.com>
2017-01-26 17:51:54 +0000

    lora: cleanup in example
    Signed-off-by: Francois Berder <francois.berder@imgtec.com>

--- examples/lora/main.c
+++ examples/lora/main.c
@@ -1,26 +1,28 @@
 #include <stdio.h>
 #include <string.h>
+#include <unistd.h>
 #include <letmecreate/letmecreate.h>
+
 
 static void receive(void)
 {
-    char *cmd = "sys get ver\r\n";
-    uint8_t tmp;
-    lora_click_receive(&tmp, sizeof(tmp));
-    printf("Character received: 0x%02X\n", tmp);
+    char buffer[16];
+    lora_click_receive((uint8_t*)buffer, sizeof(buffer) - 1);
+    buffer[15] = '\0';
+    printf("Received \"%s\"\n", buffer);
 }
 
-static void send(void)
+static void send(unsigned int n)
 {
-    static int n = 0;
-    char buffer[30];
-    sprintf(buffer, "message %u", n);
-    lora_click_send(buffer, strlen(buffer));
-    ++n;
+    char buffer[255];
+    sprintf(buffer, "Hello, World! %u", n);
+    lora_click_send((uint8_t*)buffer, strlen(buffer));
+    printf("Sent \"%s\"\n", buffer);
 }
 
 int main(int argc, char** argv)
 {
+    uint32_t n = 0;
     char mode;
     if (argc < 2) {
         fprintf(stderr, "%s r|s\n", argv[0]);
@@ -36,15 +38,17 @@ int main(int argc, char** argv)
     uart_init();
     uart_select_bus(MIKROBUS_1);
     uart_set_baudrate(UART_BD_57600);
-    lora_click_init(lora_click_get_default_configuration());
+    lora_click_init(MIKROBUS_1, lora_click_get_default_configuration());
 
-     while (1) {
+    while (1) {
         if (mode == 's') {
-            send();
+            send(n);
             sleep(1);
         }
         else if (mode == 'r')
             receive();
+
+        ++n;
     }
 
     uart_release();
