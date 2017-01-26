89791cbaa213bed3ede36429cd2f771528da77b4
Francois Berder <francois.berder@imgtec.com>
2017-01-26 10:41:38 +0000

    lora: Add doxygen documentation
    Signed-off-by: Francois Berder <francois.berder@imgtec.com>

--- include/letmecreate/click/lora.h
+++ include/letmecreate/click/lora.h
@@ -67,13 +67,91 @@ struct lora_click_config {
     bool enable_crc_header;
 };
 
+/**
+ * @return Default Configuration with following values:
+ *      frequency = 868000000
+ *      spreading_factor = 12
+ *      auto_freq_band = LORA_CLICK_AUTO_FREQ_BAND_125KHZ
+ *      coding_rate = LORA_CLICK_CODING_RATE_4_8
+ *      bandwidth = LORA_CLICK_BANDWIDTH_250KHZ
+ *      power = 14
+ *      bitrate = 5000
+ *      freq_deviation = 5000
+ *      preamble_length = 8
+ *      enable_crc_header = true
+ */
 struct lora_click_config LETMECREATE_CLICK_EXPORT lora_click_get_default_configuration(void);
+
+/**
+ * @brief Initialize the Lora Click and configure it.
+ *
+ * @param[in] config Configuration of the Lora Click
+ * @return 0 if successful, -1 otherwise
+ */
 int LETMECREATE_CLICK_EXPORT lora_click_init(struct lora_click_config config);
+
+/**
+ * @brief Configure the Lora Click
+ *
+ * @param[in] config Configuration of the Lora Click
+ * @return 0 if successful, -1 otherwise
+ */
 int LETMECREATE_CLICK_EXPORT lora_click_configure(struct lora_click_config config);
+
+/**
+ * @brief Send bytes
+ *
+ * This call is blocking, it will not return until all bytes have been sent.
+ *
+ * @param[in] data Array of bytes to send (must not be null)
+ * @param[in] count Number of bytes to send
+ * @return number of bytes sent, -1 otherwise
+ */
 int LETMECREATE_CLICK_EXPORT lora_click_send(uint8_t *data, uint32_t count);
+
+/**
+ * @brief Receive bytes
+ *
+ * This call is blocking, it will not return until @p count bytes have been received.
+ *
+ * @param[out] data
+ * @param[in] count Number of bytes to receive
+ * @return 0 if successful, -1 otherwise
+ */
 int LETMECREATE_CLICK_EXPORT lora_click_receive(uint8_t *data, uint32_t count);
+
+/**
+ * @brief Write bytes to EEPROM
+ *
+ * The RN2483 chip has 256 bytes of EEPROM located in 0x300-0x3FF range.
+ *
+ * @param[in] start_address (must be in range 0x300, 0x3FF)
+ * @param[in] data Array of bytes (must not be null)
+ * @param[in] length Number of bytes to write (start_address + length must not exceed 0x3FF)
+ * @return Number of bytes written to the EEPROM, -1 otherwise
+ */
 int LETMECREATE_CLICK_EXPORT lora_click_write_eeprom(uint32_t start_address, const uint8_t *data, uint32_t length);
+
+/**
+ * @brief Read bytes from EEPROM
+ *
+ * The RN2483 chip has 256 bytes of EEPROM located in 0x300-0x3FF range.
+ *
+ * @param[in] start_address (must be in range 0x300, 0x3FF)
+ * @param[out] data Array of bytes (must not be null)
+ * @param[in] length Number of bytes to read (start_address + length must not exceed 0x3FF)
+ * @return Number of bytes read from the EEPROM, -1 otherwise
+ */
 int LETMECREATE_CLICK_EXPORT lora_click_read_eeprom(uint32_t start_address, uint8_t *data, uint32_t length);
+
+/**
+ * @brief Read EUI of the Lora Click
+ *
+ * Each RN2483 has a unique read-only EUI.
+ *
+ * @param[out] eui Array of bytes of length 8 (must not be null)
+ * @return 0 if successful, -1 otherwise
+ */
 int LETMECREATE_CLICK_EXPORT lora_click_get_eui(uint8_t *eui);
 
 #endif
