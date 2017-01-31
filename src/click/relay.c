a8aa1d309876ca0c2768cd0e16a973d314916d6d
Michal Tusnio <m.tusnio@gmail.com>
2017-01-31 17:09:58 +0000

    relay: use gpio_get_pin instead of find_gpio
    Signed-off-by: Michal Tusnio <m.tusnio@gmail.com>

--- src/click/relay.c
+++ src/click/relay.c
@@ -4,25 +4,6 @@
 #include <letmecreate/core/gpio.h>
 
 
-static uint8_t find_gpio(uint8_t mikrobus_index)
-{
-    uint8_t gpio_pin = 0;
-
-    switch (mikrobus_index) {
-    case MIKROBUS_1:
-        gpio_pin = MIKROBUS_1_PWM;
-        break;
-    case MIKROBUS_2:
-        gpio_pin = MIKROBUS_2_PWM;
-        break;
-    default:
-        fprintf(stderr, "relay: Invalid mikrobus index.\n");
-        break;
-    }
-
-    return gpio_pin;
-}
-
 int relay_click_enable_relay_1(uint8_t mikrobus_index)
 {
     return relay_click_set_relay_1_state(mikrobus_index, 1);
@@ -35,11 +16,10 @@ int relay_click_disable_relay_1(uint8_t mikrobus_index)
 
 int relay_click_set_relay_1_state(uint8_t mikrobus_index, uint8_t state)
 {
-    uint8_t gpio_pin = find_gpio(mikrobus_index);
-    if (gpio_pin == 0)
-        return -1;
+    uint8_t gpio_pin;
 
-    if (gpio_init(gpio_pin) < 0
+    if (gpio_get_pin(mikrobus_index, TYPE_PWM, &gpio_pin) < 0
+    ||  gpio_init(gpio_pin) < 0
     ||  gpio_set_direction(gpio_pin, GPIO_OUTPUT) < 0
     ||  gpio_set_value(gpio_pin, !!state) < 0)
         return -1;
@@ -49,8 +29,8 @@ int relay_click_set_relay_1_state(uint8_t mikrobus_index, uint8_t state)
 
 int relay_click_get_relay_1_state(uint8_t mikrobus_index, uint8_t *state)
 {
-    uint8_t gpio_pin = find_gpio(mikrobus_index);
-    if (gpio_pin == 0)
+    uint8_t gpio_pin;
+    if (gpio_get_pin(mikrobus_index, TYPE_PWM, &gpio_pin) < 0)
         return -1;
 
     if (state == NULL) {
